047f574ef4d1c65678abdd3b1ea7513f090b4333
sig <dan@1up.digital>
2021-12-05 15:52:46 +0000

    improve sketch

--- src/swapchat.test.ts
+++ src/swapchat.test.ts
@@ -5,13 +5,19 @@ let token;
 test("alan session initialise", async () => {
   const session = await initialise(beeGateway);
 
+  //create own keypair
+  //create shared keypair
+  //prepare token (shared keypair privatekey, own publickey)
+
   token = session.getToken();
 
+  //set up received callback
   session.receive(() => {
     session.received.count == 1;
     session.received[0].plainText = "hi alan";
   });
 
+  //check index 0 of shared feed on timer, if index 0 is noted and receipient public key is retrieved, shared secret is calculated, index 1 is sent as ack
   await session.connected();
 
   session.send("hi becky");
@@ -19,12 +25,17 @@ test("alan session initialise", async () => {
 
 test("becky session initialise", async () => {
   let session = await respond(beeGateway, token);
+  //create own keypair
+  //shared secret is calculated
+  //prepare chunk for index 0 of shared feed, send chunk
 
+  //set up received callback
   session.received(() => {
     session.received.count == 1;
     session.received[0].plainText = "hi becky";
   });
 
+  //check index 1 of shared feed on timer, if index 1 is noted, connected
   await session.connected();
 
   session.send("hi alan");
